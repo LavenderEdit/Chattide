212475dd7ad9322254e8aa212743249bc291a466
LavenderEdit <gercermagden@gmail.com>
2026-01-13 22:03:49 -0500

    feat(main): Inicialice el archivo principal del proyecto para activar la auditoria.

--- src/main/java/studios/tkoh/chattide/ChattideApplication.java
+++ src/main/java/studios/tkoh/chattide/ChattideApplication.java
@@ -2,8 +2,10 @@ package studios.tkoh.chattide;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ChattideApplication {
 
     public static void main(String[] args) {
