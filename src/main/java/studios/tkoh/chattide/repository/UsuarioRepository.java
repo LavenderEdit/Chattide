44ddbcc245f20213f73ac992c5a6376dd2b5a0b1
LavenderEdit <gercermagden@gmail.com>
2026-01-21 10:03:35 -0500

    feat(repository): agregar UsuarioRepository con búsqueda y verificación de correo

---
+++ src/main/java/studios/tkoh/chattide/repository/UsuarioRepository.java
@@ -0,0 +1,25 @@
+package studios.tkoh.chattide.repository;
+
+import java.util.Optional;
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import studios.tkoh.chattide.model.Usuario;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByCorreo(String correo);
+
+    boolean existsByCorreo(String correo);
+
+    @Query("SELECT u FROM Usuario u WHERE lower(u.nombre) LIKE lower(concat('%', :query, '%')) OR lower(u.apellido) LIKE lower(concat('%', :query, '%'))")
+    Page<Usuario> buscarUsuarios(@Param("query") String query, Pageable pageable);
+}
