ad01f7868c227c74130946a600b6a2b9e83487fc
LavenderEdit <gercermagden@gmail.com>
2026-01-21 10:01:37 -0500

    feat(repository): agregar ComentarioRepository con consultas por publicación

---
+++ src/main/java/studios/tkoh/chattide/repository/ComentarioRepository.java
@@ -0,0 +1,20 @@
+package studios.tkoh.chattide.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import studios.tkoh.chattide.model.Comentario;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+@Repository
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+
+    // Cargar comentarios de un post
+    List<Comentario> findByPublicacionIdOrderByFechaCreacionAsc(Long publicacionId);
+
+    // Contar comentarios (para mostrar "5 comentarios" en la tarjeta del post)
+    long countByPublicacionId(Long publicacionId);
+}
