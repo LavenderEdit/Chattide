a8a9fcc66158084cc93972d7f4cddd0b96e22b3d
LavenderEdit <gercermagden@gmail.com>
2026-01-21 10:01:53 -0500

    feat(repository): agregar GrupoRepository con búsqueda y listado por creador

---
+++ src/main/java/studios/tkoh/chattide/repository/GrupoRepository.java
@@ -0,0 +1,20 @@
+package studios.tkoh.chattide.repository;
+
+import java.util.List;
+import org.springframework.boot.data.autoconfigure.web.DataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import studios.tkoh.chattide.model.Grupo;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+@Repository
+public interface GrupoRepository extends JpaRepository<Grupo, Long> {
+
+    Page<Grupo> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+
+    List<Grupo> findByCreadorId(Long creadorId);
+}
