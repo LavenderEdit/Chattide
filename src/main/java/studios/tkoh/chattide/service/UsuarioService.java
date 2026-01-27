a655d29ce5de9cc547b0adbd34b92c8d207a4e62
LavenderEdit <gercermagden@gmail.com>
2026-01-26 20:31:37 -0500

    feat(usuario): add avatar upload support to user service

--- src/main/java/studios/tkoh/chattide/service/UsuarioService.java
+++ src/main/java/studios/tkoh/chattide/service/UsuarioService.java
@@ -2,6 +2,7 @@ package studios.tkoh.chattide.service;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
 import studios.tkoh.chattide.dto.request.BusquedaRequest;
 import studios.tkoh.chattide.dto.request.UsuarioUpdateRequest;
 import studios.tkoh.chattide.dto.response.UsuarioResponse;
@@ -15,6 +16,8 @@ public interface UsuarioService {
     UsuarioResponse obtenerPerfil(Long usuarioId);
 
     UsuarioResponse actualizarPerfil(Long usuarioId, UsuarioUpdateRequest request);
+
+    UsuarioResponse actualizarAvatar(Long usuarioId, MultipartFile file);
 
     Page<UsuarioResponse> buscarUsuarios(BusquedaRequest request, Pageable pageable);
 }
