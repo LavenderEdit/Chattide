7ff661122039b8c549bf11cb03070b4966dabf5b
LavenderEdit <gercermagden@gmail.com>
2026-01-27 15:45:37 -0500

    refactor(impl): Modificación del metodo agregar comentario
    Añadido de lógica que mantiene el flujo de envio por colas amqp y
    ademas añade un servicio de notificaciones mediante websockets con
    STOMP.

--- src/main/java/studios/tkoh/chattide/service/impl/ComentarioServiceImpl.java
+++ src/main/java/studios/tkoh/chattide/service/impl/ComentarioServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import studios.tkoh.chattide.amqp.CommentProducer;
+import studios.tkoh.chattide.dto.event.CommentEvent;
 import studios.tkoh.chattide.dto.request.ComentarioRequest;
 import studios.tkoh.chattide.dto.response.ComentarioResponse;
 import studios.tkoh.chattide.exception.ChattideException;
@@ -23,12 +25,29 @@ public class ComentarioServiceImpl implements ComentarioService {
 
     private final ComentarioRepository comentarioRepository;
     private final ComentarioMapper comentarioMapper;
+    private final CommentProducer commentProducer;
 
     @Override
     @Transactional
     public ComentarioResponse agregarComentario(ComentarioRequest request) {
         Comentario comentario = comentarioMapper.toEntity(request);
-        return comentarioMapper.toResponse(comentarioRepository.save(comentario));
+        Comentario saved = comentarioRepository.save(comentario);
+
+        // Prepare AMQP Event
+        CommentEvent event = new CommentEvent(
+                saved.getId(),
+                saved.getPublicacion().getId(),
+                saved.getUsuario().getId(),
+                saved.getUsuario().getNombre() + " " + saved.getUsuario().getApellido(),
+                saved.getPublicacion().getUsuario().getCorreo(),
+                saved.getContenido(),
+                saved.getFechaRegistro()
+        );
+
+        // Push to RabbitMQ
+        commentProducer.sendCommentNotification(event);
+
+        return comentarioMapper.toResponse(saved);
     }
 
     @Override
